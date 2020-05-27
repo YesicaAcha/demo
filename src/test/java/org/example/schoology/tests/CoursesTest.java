587b469fdbcdbd1722ce6df63cb74698e6ae88c5
alejandrita <alejan_dra01@outlook.com>
2020-05-27 16:44:44 -0400

    replaced lines on CourseTest

--- src/test/java/org/example/schoology/tests/CoursesTest.java
+++ src/test/java/org/example/schoology/tests/CoursesTest.java
@@ -1,9 +1,6 @@
 package org.example.schoology.tests;
 
-import org.example.schoology.pages.Courses;
-import org.example.schoology.pages.Home;
-import org.example.schoology.pages.Login;
-import org.example.schoology.pages.SubMenu;
+import org.example.schoology.pages.*;
 import org.junit.Test;
 
 public class CoursesTest {
@@ -14,24 +11,10 @@ public class CoursesTest {
 		Home home = login.loginAs("carledriss+01@gmail.com", "P@ssw0rd");
 		SubMenu subMenu = home.clickMenu("Courses");
 		Courses courses = subMenu.clickMyCoursesLink();
-
-//		driver.findElement(By.cssSelector("a.create-course-btn")).click();
-//
-//		String courseName = "Test Course";
-//		driver.findElement(By.cssSelector("#edit-course-name")).sendKeys(courseName);
-//		WebElement sectionField = driver.findElement(By.cssSelector("#edit-section-name-1"));
-//		sectionField.clear();
-//		sectionField.sendKeys("Section");
-//		Select subjectArea = new Select(driver.findElement(By.cssSelector("#edit-subject-area")));
-//		subjectArea.selectByVisibleText("Mathematics");
-//		Select level = new Select(driver.findElement(By.cssSelector("#edit-grade-level-range-start")));
-//		level.selectByVisibleText("Undergraduate");
-//
-//		driver.findElement(By.cssSelector("#edit-submit")).click();
-//
-//		driver.findElement(By.xpath("//span[text()='Courses']/parent::button")).click();
-//
-//		driver.findElement(By.cssSelector("a[href='/courses']")).click();
+		CreateCourse createCourse = courses.clickCreateCourseBtn();
+		createCourse.createCourse("Test Course","Section", "Mathematics", "Undergraduate");
+		subMenu = home.clickMenu("Courses");
+		courses = subMenu.clickMyCoursesLink();
 //
 //		String courseActions = "//span[text()='%s']/ancestor::li//div[@class='action-links-unfold ']";
 //		driver.findElement(By.xpath(String.format(courseActions, courseName))).click();
